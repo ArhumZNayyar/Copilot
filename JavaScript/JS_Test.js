0c85d650e8a590a3a0de8aba1062a3cfd536846a
Arhum Z. Nayyar <47288337+ArhumZNayyar@users.noreply.github.com>
2021-10-26 00:08:55 -0400

    Create JS_Test.js
    Copilot initial JavaScript test

---
+++ JavaScript/JS_Test.js
@@ -0,0 +1,7 @@
+function calculateDaysBetweenDates(beginDate, endDate) {
+    var date1 = new Date(beginDate);
+    var date2 = new Date(endDate);
+    var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    return diffDays;
+}
